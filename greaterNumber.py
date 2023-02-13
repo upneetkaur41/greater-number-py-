0a23bbd6b58d209d404bf79a26f834fb2dd358a7
upneetkaur41 <123930263+upneetkaur41@users.noreply.github.com>
2023-02-13 11:18:24 +0530

    Add files via upload

---
+++ greaterNumber.py
@@ -0,0 +1,19 @@
+
+#TO FIND THE GREATEST NUMBER OUT OF ALL
+
+number1=input("Enter your number<10= \n")
+number2=input("Enter your second number<10= \n")
+number3=input("Enter your 3rd number<10= \n")
+
+if number1>number2 and number1>number3: 
+    print(number1, "is greatest")
+
+elif number2>number1 and number2>number3:
+    print(number2, "is greatest")
+
+else:
+    print(number3, "is greatest")
+
+
+
+
